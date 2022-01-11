287c9c34c306a1a27647da6adcc81d51f988a44f
panpaxia <58813776+panpaxia@users.noreply.github.com>
2022-01-11 15:23:41 +0800

    Add files via upload

---
+++ ADC/obj/RVMSIS/core_riscv.d
@@ -0,0 +1,2 @@
+RVMSIS/core_riscv.o: \
+ D:/share/test/CH573EVT/EVT/EXAM/SRC/RVMSIS/core_riscv.c
---
+++ ADC/obj/StdPeriphDriver/subdir.mk
@@ -0,0 +1,56 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_adc.c \
+D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_clk.c \
+D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_gpio.c \
+D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_pwr.c \
+D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_sys.c \
+D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_uart0.c \
+D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_uart1.c 
+
+OBJS += \
+./StdPeriphDriver/CH57x_adc.o \
+./StdPeriphDriver/CH57x_clk.o \
+./StdPeriphDriver/CH57x_gpio.o \
+./StdPeriphDriver/CH57x_pwr.o \
+./StdPeriphDriver/CH57x_sys.o \
+./StdPeriphDriver/CH57x_uart0.o \
+./StdPeriphDriver/CH57x_uart1.o 
+
+C_DEPS += \
+./StdPeriphDriver/CH57x_adc.d \
+./StdPeriphDriver/CH57x_clk.d \
+./StdPeriphDriver/CH57x_gpio.d \
+./StdPeriphDriver/CH57x_pwr.d \
+./StdPeriphDriver/CH57x_sys.d \
+./StdPeriphDriver/CH57x_uart0.d \
+./StdPeriphDriver/CH57x_uart1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+StdPeriphDriver/CH57x_adc.o: D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_adc.c
+	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused  -g -DDEBUG=1 -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\StdPeriphDriver\inc" -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\RVMSIS" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+StdPeriphDriver/CH57x_clk.o: D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_clk.c
+	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused  -g -DDEBUG=1 -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\StdPeriphDriver\inc" -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\RVMSIS" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+StdPeriphDriver/CH57x_gpio.o: D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_gpio.c
+	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused  -g -DDEBUG=1 -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\StdPeriphDriver\inc" -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\RVMSIS" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+StdPeriphDriver/CH57x_pwr.o: D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_pwr.c
+	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused  -g -DDEBUG=1 -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\StdPeriphDriver\inc" -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\RVMSIS" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+StdPeriphDriver/CH57x_sys.o: D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_sys.c
+	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused  -g -DDEBUG=1 -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\StdPeriphDriver\inc" -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\RVMSIS" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+StdPeriphDriver/CH57x_uart0.o: D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_uart0.c
+	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused  -g -DDEBUG=1 -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\StdPeriphDriver\inc" -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\RVMSIS" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+StdPeriphDriver/CH57x_uart1.o: D:/share/test/CH573EVT/EVT/EXAM/SRC/StdPeriphDriver/CH57x_uart1.c
+	@	@	riscv-none-embed-gcc -march=rv32imac -mabi=ilp32 -mcmodel=medany -msmall-data-limit=8 -mno-save-restore -Os -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-common -Wunused  -g -DDEBUG=1 -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\StdPeriphDriver\inc" -I"D:\share\test\CH573EVT\EVT\EXAM\SRC\RVMSIS" -std=gnu99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@	@
+
